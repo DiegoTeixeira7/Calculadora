5d526aa1c79cc953f324eea1e48f610b3fb40476
DiegoTeixeira7 <dieg07teixeira@outlook.com>
2020-09-25 21:50:03 -0300

    backend calculadora completa, finalizado

--- app/src/main/java/com/example/diegoteixeira/calculadora/CompleteCalc.java
+++ app/src/main/java/com/example/diegoteixeira/calculadora/CompleteCalc.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
+import java.text.DecimalFormat;
+
 import static java.lang.Double.parseDouble;
 
 public class CompleteCalc extends AppCompatActivity {
@@ -50,19 +52,79 @@ public class CompleteCalc extends AppCompatActivity {
         valor.setText(novoValor);
     }
 
+    private boolean validaSub(String exp){
+        String valida = exp;
+        boolean resposta = false;
+        if(exp.length() > 1) {
+            if(exp.substring(0,1).equals("-")) {
+                valida = exp.substring(1);
+            }
+
+            int index = valida.indexOf("-");
+
+            if(index == -1) {
+                resposta = true;
+            } else {
+                if(valida.length() == index+1){
+                    resposta = true;
+
+                } else {
+                    if(!valida.substring(index+1,index+2).equals("-")) {
+                        resposta = true;
+                    }
+                }
+            }
+        } else {
+            if(valida.length() == 0) {
+                resposta = true;
+            } else if(valida.length() == 1 && !valida.equals("-")) {
+                resposta = true;
+            }
+        }
+
+        return resposta;
+    }
+
     private int validaExpressao(String exp) {
         int index = exp.indexOf("+");
         if(index == -1) {
-            index = exp.indexOf("-");
+            index = exp.indexOf("*");
             if(index == -1) {
-                index = exp.indexOf("*");
-                if(index == -1) {
-                    index = exp.indexOf("/");
+                index = exp.indexOf("/");
+                if(index == -1 && exp.length() > 1) {
+                    if(exp.substring(0,1).equals("-")){
+                        index = exp.substring(1).indexOf("-");
+                    } else {
+                        index = exp.indexOf("-");
+                    }
                 }
             }
         }
         return index;
     }
+
+    private int validaConta(String exp) {
+        int index = exp.indexOf("+");
+        if(index == -1) {
+            index = exp.indexOf("*");
+            if(index == -1) {
+                index = exp.indexOf("/");
+                if(index == -1 && exp.length() > 1) {
+                    if(exp.substring(0,1).equals("-")){
+                        if(exp.substring(1).indexOf("-") == -1) {
+                            index = -1;
+                        } else {
+                            index = exp.substring(1).indexOf("-") + 1;
+                        }
+                    } else {
+                        index = exp.indexOf("-");
+                    }
+                }
+            }
+        }
+        return index;
+    }
+
 
     private int validaPonto(String exp) {
         int index = validaExpressao(exp);
@@ -92,7 +154,7 @@ public class CompleteCalc extends AppCompatActivity {
                     }
                 } else if (tag.equals("mais") && (validaExpressao(valor.getText().toString()) == -1)) {
                     novoValor = valor.getText().toString() + "+";
-                } else if (tag.equals("sub") && (validaExpressao(valor.getText().toString()) == -1)) {
+                } else if (tag.equals("sub") && (validaExpressao(valor.getText().toString()) == -1) && validaSub(valor.getText().toString())) {
                     novoValor = valor.getText().toString() + "-";
                 } else if (tag.equals("multi") && (validaExpressao(valor.getText().toString()) == -1)) {
                     novoValor = valor.getText().toString() + "*";
@@ -103,18 +165,23 @@ public class CompleteCalc extends AppCompatActivity {
             } else {
                 String atual = valor.getText().toString();
 
-                if (tag.equals("ponto")) {
+                if (tag.equals("ponto") && atual.length() > 1 && !atual.substring(atual.length()-1).equals("-")) {
                     if(atual.indexOf(".") == -1 ){
                         novoValor = atual.substring(0,atual.length()-1) + ".";
                     }
-                } else if (tag.equals("mais") && (validaExpressao(atual.substring(0,atual.length()-1)) == -1)) {
-                    novoValor = atual.substring(0,atual.length()-1) + "+";
-                } else if (tag.equals("sub") && (validaExpressao(atual.substring(0,atual.length()-1)) == -1)) {
-                    novoValor = atual.substring(0,atual.length()-1) + "-";
-                } else if (tag.equals("multi") && (validaExpressao(atual.substring(0,atual.length()-1)) == -1)) {
-                    novoValor = atual.substring(0,atual.length()-1) + "*";
-                } else if (tag.equals("div") && (validaExpressao(atual.substring(0,atual.length()-1)) == -1)) {
-                    novoValor = atual.substring(0,atual.length()-1) + "/";
+                } else {
+                    boolean p = !atual.substring(atual.length() - 1).equals(".");
+                    if (tag.equals("mais") && (validaExpressao(atual.substring(0,atual.length()-1)) == -1) && (atual.length() > 1) && p) {
+                        novoValor = atual.substring(0,atual.length()-1) + "+";
+                    } else if (tag.equals("sub") && (validaExpressao(atual.substring(0,atual.length()-1)) == -1) && validaSub(valor.getText().toString()) && p) {
+
+                            novoValor = atual + "-";
+
+                    } else if (tag.equals("multi") && (validaExpressao(atual.substring(0,atual.length()-1)) == -1) && (atual.length() > 1) && p ){
+                        novoValor = atual.substring(0,atual.length()-1) + "*";
+                    } else if (tag.equals("div") && (validaExpressao(atual.substring(0,atual.length()-1)) == -1) && (atual.length() > 1) && p) {
+                        novoValor = atual.substring(0,atual.length()-1) + "/";
+                    }
                 }
             }
 
@@ -122,12 +189,118 @@ public class CompleteCalc extends AppCompatActivity {
                 valor.setText(novoValor);
             }
 
+        } else {
+            if(tag.equals("sub")){
+                valor.setText("-");
+            }
+        }
+    }
+
+    private boolean typeValor(double valor) {
+        if(valor % 1 == 0){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private int arredondar(String v1, String v2) {
+        int index1 = v1.indexOf(".");
+        int index2 = v2.indexOf(".");
+        int t1 = 0;
+        int t2 = 0;
+
+        if(index1 != -1) {
+            t1 = v1.length() - 1 - index1;
         }
 
+        if(index2 != -1) {
+            t2 = v2.length() - 1 - index2;
+        }
+
+        if(t1 > t2) {
+            return t1;
+        } else if(t2 > t1) {
+            return t2;
+        } else {
+            return t1;
+        }
+    }
+
+    private String findVirgula(String valor) {
+        int index = valor.indexOf(",");
+        String resp = "";
+        if(index != -1) {
+            resp = valor.substring(0,index)+"."+valor.substring(index+1);
+            return resp;
+        }
+
+        return valor;
     }
 
     public void setFunc(View view) {
+        valor = findViewById(R.id.valor);
+        String atual = valor.getText().toString();
 
+        String tag = view.getTag().toString();
+
+        if(tag.equals("delete")) {
+            if(atual.length() > 0) {
+                valor.setText(atual.substring(0,atual.length()-1));
+            }
+        } else if(tag.equals("deleteAll")) {
+            valor.setText("");
+        } else if(tag.equals("igual")) {
+
+            if((atual.length() > 2)){
+                int index = validaConta(atual);
+
+                if(index != -1 && index != atual.length()-1) {
+                    String valor1 = atual.substring(0,index);
+                    String op = atual.substring(index,index+1);
+                    String valor2 = atual.substring(index+1);
+
+                    if(!valor2.equals("-") && !valor2.substring(valor2.length()-1).equals(".")) {
+                        int arredondar = arredondar(valor1, valor2);
+
+                        String zero = "";
+                        for(int i=0;i<arredondar;i++) {
+                            zero+="0";
+                        }
+
+                        if(op.equals("+")) {
+                            double resp = parseDouble(valor1) + parseDouble(valor2);
+                            if(typeValor(resp)) {
+                                valor.setText(String.valueOf((int)resp));
+                            } else {
+                                valor.setText(findVirgula(new DecimalFormat("#,##0." + zero).format(resp)));
+                            }
+                        } else if(op.equals("-")) {
+                            double resp = parseDouble(valor1) - parseDouble(valor2);
+                            if(typeValor(resp)) {
+                                valor.setText(String.valueOf((int)resp));
+                            } else {
+                                valor.setText(findVirgula(new DecimalFormat("#,##0." + zero).format(resp)));
+                            }
+                        } else if(op.equals("*")) {
+                            double resp = parseDouble(valor1) * parseDouble(valor2);
+                            if(typeValor(resp)) {
+                                valor.setText(String.valueOf((int)resp));
+                            } else {
+                                valor.setText(findVirgula(new DecimalFormat("#,##0." + zero).format(resp)));
+                            }
+                        } else if(op.equals("/") && !valor2.equals("0")){
+                            double resp = parseDouble(valor1) / parseDouble(valor2);
+                            if(typeValor(resp)) {
+                                valor.setText(String.valueOf((int)resp));
+                            } else {
+                                valor.setText(findVirgula(new DecimalFormat("#,##0." + zero).format(resp)));
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 
     public void voltaMenu(View view) {
--- app/src/main/java/com/example/diegoteixeira/calculadora/SimpleCalc.java
+++ app/src/main/java/com/example/diegoteixeira/calculadora/SimpleCalc.java
@@ -7,6 +7,8 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.text.DecimalFormat;
+
 import static java.lang.Double.parseDouble;
 import static java.lang.Integer.parseInt;
 
@@ -20,6 +22,48 @@ public class SimpleCalc extends AppCompatActivity {
         setContentView(R.layout.activity_simple_calc);
     }
 
+    private boolean typeValor(double valor) {
+        if(valor % 1 == 0){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private int arredondar(String v1, String v2) {
+        int index1 = v1.indexOf(".");
+        int index2 = v2.indexOf(".");
+        int t1 = 0;
+        int t2 = 0;
+
+        if(index1 != -1) {
+            t1 = v1.length() - 1 - index1;
+        }
+
+        if(index2 != -1) {
+            t2 = v2.length() - 1 - index2;
+        }
+
+        if(t1 > t2) {
+            return t1;
+        } else if(t2 > t1) {
+            return t2;
+        } else {
+            return t1;
+        }
+    }
+
+    private String findVirgula(String valor) {
+        int index = valor.indexOf(",");
+        String resp = "";
+        if(index != -1) {
+            resp = valor.substring(0,index)+"."+valor.substring(index+1);
+            return resp;
+        }
+
+        return valor;
+    }
+
     public void calcular(View view) {
         String valor1 = ((EditText)findViewById(R.id.valor1)).getText().toString();
         String valor2 = ((EditText)findViewById(R.id.valor2)).getText().toString();
@@ -31,17 +75,44 @@ public class SimpleCalc extends AppCompatActivity {
         if(valor1.equals("") || valor2.equals("") || valor1.equals(".") || valor1.equals("-") || valor2.equals(".") || valor2.equals("-")){
             resposta.setText("Valor inválido");
         } else {
+            int arredondar = arredondar(valor1, valor2);
+
+            String zero = "";
+            for(int i=0;i<arredondar;i++) {
+                zero+="0";
+            }
+
             if (tag.equals("sum")) {
-                resposta.setText(String.valueOf(parseDouble(valor1) + parseDouble(valor2)));
+                double resp = parseDouble(valor1) + parseDouble(valor2);
+                if(typeValor(resp)) {
+                    resposta.setText(String.valueOf((int)resp));
+                } else {
+                    resposta.setText(findVirgula(new DecimalFormat("#,##0." + zero).format(resp)));
+                }
             } else if (tag.equals("dec")) {
-                resposta.setText(String.valueOf(parseDouble(valor1) - parseDouble(valor2)));
+                double resp = parseDouble(valor1) - parseDouble(valor2);
+                if(typeValor(resp)) {
+                    resposta.setText(String.valueOf((int)resp));
+                } else {
+                    resposta.setText(findVirgula(new DecimalFormat("#,##0." + zero).format(resp)));
+                }
             } else if (tag.equals("multi")) {
-                resposta.setText(String.valueOf(parseDouble(valor1) * parseDouble(valor2)));
+                double resp = parseDouble(valor1) * parseDouble(valor2);
+                if(typeValor(resp)) {
+                    resposta.setText(String.valueOf((int)resp));
+                } else {
+                    resposta.setText(findVirgula(new DecimalFormat("#,##0." + zero).format(resp)));
+                }
             } else if (tag.equals("div")) {
                 if ((parseDouble(valor2)) == 0) {
                     resposta.setText("Divisão por zero");
                 } else {
-                    resposta.setText(String.valueOf(parseDouble(valor1) / parseDouble(valor2)));
+                    double resp = parseDouble(valor1) / parseDouble(valor2);
+                    if(typeValor(resp)) {
+                        resposta.setText(String.valueOf((int)resp));
+                    } else {
+                        resposta.setText(findVirgula(new DecimalFormat("#,##0." + zero).format(resp)));
+                    }
                 }
             }
         }
